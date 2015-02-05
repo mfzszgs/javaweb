dbe29fc4b6ae2e573bd3d86664391e2373067a73
mfzszgs <su_wy@outlook.com>
2015-02-05 18:41:45 +0800

    a

--- eclipse/Copy of DbServiceTest/src/test/client/MysqlConnectionClient.java
+++ eclipse/Copy of DbServiceTest/src/test/client/MysqlConnectionClient.java
@@ -20,63 +20,61 @@ public class MysqlConnectionClient {
 	private static final String PASSWORD = "s";
 
 	public static void main(String args[]) throws Exception {
+		System.out.println("!!!!!!!!!!!!!!");
 		URL url = new URL("http://localhost:10101/mysql?wsdl");
 
-		// Qualified name of the service:
-		// 1st arg is the service URI
-		// 2nd is the service name published in the WSDL
 		QName qname = new QName("http://server.test/",
 				"ConnectionDelegationImplService");
-  
-		// Create, in effect, a factory for the service.
+
 		Service service = Service.create(url, qname);
 
-		// Extract the endpoint interface, the service "port".
-		
-//		MysqlConnectionServer db = service.getPort(MysqlConnectionServer.class);
 		ConnectionDelegation conn = service.getPort(ConnectionDelegation.class);
-		
+		System.out.println("!!!!!!!!!!!!!!");
 		try {
-		conn.getConnect(DRIVER, URL, USER, PASSWORD);
+			System.out.println("a");
+			conn.getConnect(DRIVER, URL, USER, PASSWORD);
+			System.out.println("b");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		if(conn!=null)System.out.println("!NULL");
-		int i=1;
+		System.out.println("!!!!!!!!!!!!!!");
+		if (conn != null)
+			System.out.println("!NULL");
+		int i = 1;
 		List<String> li = new ArrayList<String>();
 		ConnectionDelegation.StatementDelegation stmt = null;
 		ConnectionDelegation.ResultSetDelegation rs = null;
 		String sql = null;
 		try {
-		if (!conn.isClosed())
-			System.out.println("Succeeded connecting to the Database!");
+			if (!conn.isClosed())
+				System.out.println("Succeeded connecting to the Database!");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		try {
-			if(i == 0){
+			if (i == 0) {
 				sql = "select ColumnName from newscolumn";
-			}
-			else{
-				sql = "select ColumnName from newscolumn where id = '"+i+"'";
+			} else {
+				sql = "select ColumnName from newscolumn where id = '" + i
+						+ "'";
 			}
 			stmt = conn.createStatement();
-			rs = stmt.executeQuery(sql);//要执行的SQL语句
-			while(rs.next()){
+			rs = stmt.executeQuery(sql);// 要执行的SQL语句
+			while (rs.next()) {
 				li.add(rs.getString("ColumnName"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-//  			conn.closeRs();
-//			conn.closeStmt();
-//			conn.close();
+			// conn.closeRs();
+			// conn.closeStmt();
+			// conn.close();
 
 			rs.close();
 			stmt.close();
 			conn.close();
 			System.out.println(li);
 		}
-		
+
 	}
 }
